d5800a43d5771ba6b396e79e54857ffd7248f5e9
Anupam Kumar <anupamk3@cisco.com>
2025-07-08 14:00:30 +0530

    Another printout as per comment.

---
+++ src/com/learner/stringtest/StringTest2.java
@@ -0,0 +1,13 @@
+package com.learner.stringtest;
+
+public class StringTest2 {
+
+    public static void main(String[] args) {
+
+        String str = "Hi";
+
+        System.out.println(str);
+
+    }
+    
+}
