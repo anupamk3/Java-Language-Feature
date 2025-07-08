5961c3ca45f28573606f7aff472f86a4a5e33164
Anupam Kumar <anupamk3@cisco.com>
2025-07-08 13:54:42 +0530

    Another printout as per comment.

--- src/com/learner/stringtest/StringTest1.java
+++ src/com/learner/stringtest/StringTest1.java
@@ -10,7 +10,10 @@ public class StringTest1 {
         String str2 = "World";
         System.out.println(str2);
 
-        System.out.println(str1 + " "  +str2);
+        System.out.println(str1 + " "  + str2);
+
+        System.out.println("Adding another printouts");
+
     }
 
 }
