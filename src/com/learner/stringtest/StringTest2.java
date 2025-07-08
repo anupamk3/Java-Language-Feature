f39ab00be85762ea6a7cbfadf45ae85a3aece53e
Anupam Kumar <anupamk3@cisco.com>
2025-07-08 14:10:10 +0530

    Adding StringTest2 and Hi from 1st branch.

---
+++ src/com/learner/stringtest/StringTest2.java
@@ -0,0 +1,12 @@
+package com.learner.stringtest;
+
+public class StringTest2 {
+
+    public static void main(String[] args) {
+
+        String str = "Hi";
+
+        System.out.println(str);
+
+    }
+}
