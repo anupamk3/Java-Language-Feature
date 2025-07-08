1c9f498941b043827c1f637aca9f3be1ad42caa6
Anupam Kumar <anupamk3@cisco.com>
2025-07-08 13:43:17 +0530

    Project setup in IntelliJ.

---
+++ src/com/learner/stringtest/StringTest1.java
@@ -0,0 +1,11 @@
+package com.learner.stringtest;
+
+public class StringTest1 {
+
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        System.out.println(str1);
+    }
+
+}
