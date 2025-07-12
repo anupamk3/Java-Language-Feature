ef595b2c7753641d2bdf05973b4fe98fbee57548
Anupam Kumar <anupamk3@cisco.com>
2025-07-12 13:34:17 +0530

    Revert "Merge branch 'main' into testB1"
    This reverts commit b3251c5c6eb03e14de4d87b353fc30edde042b6a, reversing
    changes made to 3effcb2d7eb497138cb432bb9fa9dce3e51cc37c.

--- src/com/learner/stringtest/StringTest3.java
+++ src/com/learner/stringtest/StringTest3.java
@@ -1,11 +1,10 @@
 package com.learner.stringtest;
 
-
 public class StringTest3 {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
-        System.out.println("Hi EveryOne hello");
-        System.out.println("Everyone");
+        System.out.println("Hi EveryOne");
+
         //Adding comment
 
         //Adding Comment 1
